8556f729888dbcf1306b321fe1a32bec1101e868
Rakesh Kundu <159798591+rax-2@users.noreply.github.com>
2024-08-20 21:33:47 +0530

    v0.9

---
+++ bluetooth_module_setup.ino
@@ -0,0 +1,26 @@
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(2, 3); // RX, TX
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
+
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(38400);
+}
+
+void loop() { // run over and over
+  if (mySerial.available()) {
+    Serial.write(mySerial.read());
+  }
+  if (Serial.available()) {
+    mySerial.write(Serial.read());
+  }
+}
